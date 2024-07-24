f874aed5a8a8b22d30d2af1e1b105d76e1d280bf
Ishwar <ishwarky15@gmail.com>
2024-07-24 10:40:09 +0530

    configuration file modified

--- src/cnnclassifier/config/configuration.py
+++ src/cnnclassifier/config/configuration.py
@@ -55,16 +55,16 @@ class configuration_manager:
 
         return training_config
     
-        def get_evaluation_config(self) -> EvaluationConfig:
-            eval_config = EvaluationConfig(
-                path_of_model="artifacts/training/model.h5",
-                training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
-                mlflow_uri="https://dagshub.com/ishwarky15/Chestcancerdetection.mlflow",
-                all_params=self.params,
-                params_image_size=self.params.IMAGE_SIZE,
-                params_batch_size=self.params.BATCH_SIZE
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+        path_of_model="artifacts/training/model.h5",
+        training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+        mlflow_uri="https://dagshub.com/ishwarky15/Chestcancerdetection.mlflow",
+        all_params=self.params,
+        params_image_size=self.params.IMAGE_SIZE,
+        params_batch_size=self.params.BATCH_SIZE
             )
-            return eval_config
+        return eval_config
     
 
     
