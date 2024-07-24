afc78de1d46897ee1ce01b03287c75e17887ee8d
Ishwar <ishwarky15@gmail.com>
2024-07-24 11:21:34 +0530

    model evaluation done

--- src/cnnclassifier/components/model_evaluation.py
+++ src/cnnclassifier/components/model_evaluation.py
@@ -50,7 +50,7 @@ class Evaluation:
 
     def save_score(self):
         scores = {"loss": self.score[0], "accuracy": self.score[1]}
-        save_json(path=Path("scores.json"), data=scores)
+        #save_json(path=Path("scores.json"), data=scores)
 
     
     def log_into_mlflow(self):
